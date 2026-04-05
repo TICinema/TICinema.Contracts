ef9bee8b0e57b230f752da455c429142e74f34d4
SolomonJSX <janserik405@outlook.com>
2026-04-05 16:11:06 +0500

    New Released Events Pack

---
+++ TICinema.Contracts/Events/OtpRequestedEvent.cs
@@ -0,0 +1,8 @@
+﻿namespace TICinema.Contracts.Events;
+
+public class OtpRequestedEvent
+{
+    public string Identifier { get; init; } = string.Empty; // Телефон или Email
+    public string Code { get; init; } = string.Empty;
+    public string Type { get; set; } = string.Empty;
+}
