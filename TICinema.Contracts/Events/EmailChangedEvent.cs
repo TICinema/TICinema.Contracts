a7d6852abad73264a29efa6a3f2617c57446f63e
SolomonJSX <janserik405@outlook.com>
2026-04-06 00:50:28 +0500

    Add event for change email

---
+++ TICinema.Contracts/Events/EmailChangedEvent.cs
@@ -0,0 +1,8 @@
+﻿namespace TICinema.Contracts.Events;
+
+public class EmailChangedEvent
+{
+    public string NewEmail { get; init; } = string.Empty;
+    public string Code { get; init; } = string.Empty;
+    public DateTime CreatedAt { get; init; } = DateTime.UtcNow;
+}
